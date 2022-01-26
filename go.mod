d6dfc4d3a34316eee773b8f1b9bb71f2fd73e036
Muhammad Yusril <muhammadyusril147@gmail.com>
2022-01-26 12:49:33 +0700

    menambah app-say-hello

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/myusrilh11/app-say-hello
+
+go 1.17
+
+require github.com/myusrilh11/go-say-hello v1.0.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	gosayhello "github.com/myusrilh11/go-say-hello"
+)
+
+func main() {
+	fmt.Println(gosayhello.SayHello())
+}
